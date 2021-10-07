c552a0c909884b0265056c9ae3d21b11dde9ddcf
장정원 <longthegarden@innodep.com>
2021-10-07 15:25:04 +0900

    [섹션2-21] 유튜브(Youtube) 검색 반환

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAoFQt8hCyBCVHjseiqzUgOxd9KruE_4AI';
+
+YTSearch({key: API_KEY, term: 'BTS'}, function(data) {
+    console.log(data);
+});
 
 // Create a component.
 const App = () => {
